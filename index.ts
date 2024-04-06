4dd016008482462580e73bdbdd30eae5bdef89df
Asad Ali <asadali30438@gmail.com>
2024-04-07 04:19:39 +0500

    Add code for generate a number by coumputer itself between range of 1 to 5

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
 
-const randomNumber = 4;
+const randomNumber = Math.floor(Math.random() * 5 );
 
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number",
+        message: "Please guess a number between 1 to 5",
     },
 ]);
 
