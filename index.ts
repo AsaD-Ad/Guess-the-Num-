81402ae8d3cc650e2b72b136e1101aa29ccf6123
Asad Ali <asadali30438@gmail.com>
2024-04-07 03:32:53 +0500

    I push my project of CLI based Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+const randomNumber = 4;
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number",
+    },
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congratulations! You guessed right number")
+}else {
+    console.log("You guessed WRONG NUM!")
+};
+
